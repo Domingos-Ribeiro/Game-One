82dbcb562ff05d757bf4fb66bdefe3ad68b6200a
Domingos Ribeiro <domingos.ribeiro.net@gmail.com>
2020-11-08 00:57:27 +0000

    BackGround

--- FirstApp/FirstApp.cs
+++ FirstApp/FirstApp.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace FirstApp
 {
-    public partial class Form1 : Form
+    public partial class FirstApp : Form
     {
-        public Form1()
+        public FirstApp()
         {
             InitializeComponent();
         }
